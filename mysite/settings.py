e7488936e738870cc30412c5aa6e98434d319c62
dansobolev <daniil.sob56@gmail.com>
2019-08-06 00:37:50 +0300

    added IP finder

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,11 +131,5 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'forum_staff01@gmail.com'
-EMAIL_HOST_PASSWORD = 'mdxlswqkqvpkkqnc'
-EMAIL_POST = 465
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = "Best regards, forum staff"
+
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -29,21 +29,6 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
 
 
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
-         name='password_change_complete'),
-
-    path('password/change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
-         name='password_change1'),
-
-    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
-         name='password_reset_done'),
-
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
-         name='password_reset_complete'),
 
     #потом переделать с
     #path('', include('blog.urls')),

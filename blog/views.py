63008784595a4d3ed7b2f8782adaaae67d0edc47
dansobolev <daniil.sob56@gmail.com>
2019-08-11 02:47:50 +0300

    some changes

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import login,authenticate
 from django.contrib.auth.forms import UserCreationForm
 from .forms import RegisterForm, PostForm, EmailForm
 from .models import Post
-from ipware import get_client_ip
+
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
 
@@ -83,8 +83,8 @@ def yourprofile(request):
         answer = "Online"
     else:
         answer = "Offline"""""
-    client_ip = get_client_ip(request)
-    return render(request, 'blog/my_profile.html', {'ip_adress': client_ip})
+
+    return render(request, 'blog/my_profile.html')
 
 
 def password_res(req):

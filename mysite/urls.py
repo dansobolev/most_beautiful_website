0023d1a20c303527435fcbc0377b3ff3839e77ab
dansobolev <daniil.sob56@gmail.com>
2019-08-12 00:27:08 +0300

    fixed

--- mysite/urls.py
+++ mysite/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
          auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
          name='password_reset_done'),
 
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
          name='password_reset_confirm'),
 
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
